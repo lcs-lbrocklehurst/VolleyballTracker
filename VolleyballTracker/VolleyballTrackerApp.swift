98349f15f1fadbb26d8988a2c2c151be693310cf
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2023-11-01 12:15:55 -0400

    Initial Commit

---
+++ VolleyballTracker/VolleyballTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolleyballTrackerApp.swift
+//  VolleyballTracker
+//
+//  Created by Lewis Brocklehurst on 2023-11-01.
+//
+
+import SwiftUI
+
+@main
+struct VolleyballTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
