f8cba9561c558b8aafaabef431f076c5f9571ce0
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2023-11-15 21:43:39 -0500

    Created the second stepper

--- VolleyballTracker/ContentView.swift
+++ VolleyballTracker/ContentView.swift
@@ -8,25 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     //MARK: Stored properties
     
     @State private var Title: String = ""
-    @State var sets: Double = 0
+    @State var setsHome: Double = 0
+    @State var setsAway: Double = 0
     
     var body: some View {
-        NavigationView() {
+        
+        
+        VStack {
             
-            VStack {
-                
-                TextField("Add a new game", text: $Title)
-                    .font(.title)
-                    .padding(.horizontal)
-                Spacer()
-                
+            TextField("Add a new game", text: $Title)
+                .font(.title)
+                .padding(.horizontal)
+            Spacer()
             
-                    
-                    .padding()
-                    
+            
+            
+                .padding()
+            
+                
                 HStack {
                     
                     Text("Home")
@@ -36,25 +39,46 @@ struct ContentView: View {
                 }
                 .padding(.horizontal)
                 
-                Stepper("\(sets.formatted(.number.precision(.fractionLength(0))))",
-                        value: $sets,
+                Stepper("\(setsHome.formatted(.number.precision(.fractionLength(0))))",
+                        value: $setsHome,
                         in: 1...3)
                 .font(.system(size: 28))
                 .padding()
                 
+                HStack {
+                    Text("Away")
+                        .font(.system(size: 28))
+                    
+                    Spacer()
+                    
+                }
                 
-                    }
-            
-            
+                .padding(.horizontal)
+                
+                HStack(spacing: 0) {
+                    
+                    Stepper("\(setsAway.formatted(.number.precision(.fractionLength(0))))",
+                            value: $setsAway,
+                            in: 1...3)
+                    .font(.system(size: 28))
+                    .padding()
+                }
+                
+                
             }
             
+            
+            
         }
+        
+        
         
         
     }
 
     
-#Preview {
+    #Preview {
         ContentView()
     }
+    
 
